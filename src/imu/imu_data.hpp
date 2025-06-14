b57db34b2988e26d139d0f68367c35490a412c92
Tofigh-Tevfik <tofigh.github@gmail.com>
2025-06-14 23:24:30 +0300

    Add IMU data, and IMUReader class

---
+++ src/imu/imu_data.hpp
@@ -0,0 +1,27 @@
+#ifndef IMU_DATA_HPP
+#define IMU_DATA_HPP
+
+#include <cstdint>
+
+// struct of accelerameter data
+struct accel {
+    int32_t x;
+    int32_t y;
+    int32_t z;
+};
+
+// struct for gyroscope data
+struct gyro {
+    int32_t x;
+    int32_t y;
+    int32_t z;
+};
+
+struct IMUData {
+    uint64_t timestamp;
+    int32_t temperature;
+    gyro gyro_data;
+    accel accel_data;
+}
+
+#endif
---
+++ src/imu/imu_reader.hpp
@@ -0,0 +1,21 @@
+#ifndef IMU_READER_HPP
+#define IMU_READER_HPP
+
+#include "imu_data.hpp"
+
+class IMUReader {
+    private:
+    IMUData imu_data;
+
+    public:
+        IMUReader();
+        virtual ~IMUReader() = 0;
+        virtual bool initialize() = 0;
+        virtual bool read_accelerometer(float& x, float& y, float& z) = 0;
+        virtual bool read_gyroscope(float& x, float& y, float& z) = 0;
+        virtual bool read_temperature(float& temp) = 0;
+        virtual bool set_gyro_range(float range) = 0; // configuration method
+        virtual bool set_accel_range(float range) = 0; // configuration method
+}
+
+#endif
