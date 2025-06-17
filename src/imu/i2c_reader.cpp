078c0243572c67f163e085d95c8a3c981dce99de
Tofigh-Tevfik <tofigh.github@gmail.com>
2025-06-17 21:14:33 +0300

    Add i2c_reader.cpp with stub implementation

---
+++ src/imu/i2c_reader.cpp
@@ -0,0 +1,45 @@
+#include "i2c_reader.hpp"
+#include <stdexcept>
+
+// stub impelementation
+I2CReader::I2CReader(const std::string& i2c_bus, uint8_t address) 
+    : fd(-1), i2c_address(address) {
+    throw::std::runtime_error("I2CReader class not implemented");
+}
+
+I2CReader::~I2CReader() {}
+
+bool I2CReader::initialize() {
+    return false;
+}
+
+bool I2CReader::read_accelerometer(float& x, float& y, float& z) {
+    x = y = z = 0.f;
+    return false;
+}
+
+bool I2CReader::read_gyroscope(float& x, float& y, float& z) {
+    x = y = z = 0.f;
+    return false;
+}
+
+bool I2CReader::read_temperature(float& temp) {
+    temp = 0.f;
+    return false;
+}
+
+bool I2CReader::set_gyro_range(float range) {
+    return false;
+}
+
+bool I2CReader::set_accel_range(float range) {
+    return false;
+}
+
+bool I2CReader::write_register(uint8_t reg, uint8_t value) {
+    return false;
+}
+
+bool I2CReader::read_register(uint8_t reg, uint8_t* data, size_t len) {
+    return false;
+}
